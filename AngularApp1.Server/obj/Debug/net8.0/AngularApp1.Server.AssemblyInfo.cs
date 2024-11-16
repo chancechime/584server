34f384ec2c54ef7e5d0e01a5073a0c47fba22871
Chance Chime <chance.chime.814@my.csun.edu>
2024-11-15 17:43:56 -0800

    11/15: Update assembly version in AngularApp1.Server and DataModel projects

--- AngularApp1.Server/obj/Debug/net8.0/AngularApp1.Server.AssemblyInfo.cs
+++ AngularApp1.Server/obj/Debug/net8.0/AngularApp1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AngularApp1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34019c72acd11b4cfefa74b395f7a872c082d8bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fe244a9c3c04bad6f4e5d517fd27a991025cf24")]
 [assembly: System.Reflection.AssemblyProductAttribute("AngularApp1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AngularApp1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
